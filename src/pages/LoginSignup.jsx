f8f0e4cef5454271db632565ddcd4a1dd6ca59da
Ankit <ankitkeshri0727@gmail.com>
2025-03-18 20:25:36 +0530

    updated

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -9,7 +9,7 @@ const AuthForm = () => {
       style={{ marginTop: "-100px" }}
     >
       <div
-        className="bg-white rounded-lg shadow-lg w-96"
+        className="bg-gray-900 text-white rounded-lg shadow-lg w-96"
         style={{ padding: "32px" }}
       >
         <h2
@@ -25,8 +25,8 @@ const AuthForm = () => {
           style={{ padding: "4px", marginBottom: "24px" }}
         >
           <button
-            className={`flex-1 rounded-md font-medium transition ${
-              isLogin ? "bg-blue-600 text-white" : "text-gray-600"
+            className={`flex-1 rounded-md font-medium transition cursor-pointer ${
+              isLogin ? "bg-black text-gray-400" : "text-gray-600"
             }`}
             style={{ paddingTop: "8px", paddingBottom: "8px" }}
             onClick={() => setIsLogin(true)}
@@ -34,8 +34,10 @@ const AuthForm = () => {
             Login
           </button>
           <button
-            className={`flex-1 rounded-md font-medium transition ${
-              !isLogin ? "bg-blue-600 text-white" : "text-gray-600"
+            className={`cursor-pointer flex-1 rounded-md font-medium transition ${
+              !isLogin
+                ? "bg-black text-gray-400"
+                : "text-gray-600"
             }`}
             style={{ paddingTop: "8px", paddingBottom: "8px" }}
             onClick={() => setIsLogin(false)}
@@ -49,7 +51,7 @@ const AuthForm = () => {
           <input
             type="email"
             placeholder="Email Address"
-            className="w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            className="w-full border bg-black border-gray-400 rounded-md focus:outline-none focus:border-gray-900"
             style={{
               paddingTop: "8px",
               paddingBottom: "8px",
@@ -61,7 +63,7 @@ const AuthForm = () => {
           <input
             type="password"
             placeholder="Password"
-            className="w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+            className="w-full border bg-black border-gray-400 rounded-md focus:outline-none focus:border-gray-900"
             style={{
               paddingTop: "8px",
               paddingBottom: "8px",
@@ -76,7 +78,7 @@ const AuthForm = () => {
             <input
               type="password"
               placeholder="Confirm password"
-              className="w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+              className="w-full bg-black border border-gray-400 rounded-md focus:outline-none focus:border-gray-900"
               style={{
                 paddingTop: "8px",
                 paddingBottom: "8px",
@@ -88,7 +90,7 @@ const AuthForm = () => {
           )}
 
           <button
-            className="w-full bg-blue-600 text-white rounded-md text-lg font-semibold shadow-md transition hover:bg-blue-700"
+            className="w-full bg-black text-white rounded-md text-lg font-semibold shadow-md transition hover:bg-gray-700 cursor-pointer"
             type="submit"
             style={{ paddingTop: "12px", paddingBottom: "12px" }}
           >
